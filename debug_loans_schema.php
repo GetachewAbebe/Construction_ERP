cd4e9dd04a3aa4704a3a9879022c13a46852a3bf
GetachewAbebe <gechachin@gmail.com>
2025-12-08 19:09:49 +0300

    Deleting Unneccsary Files

--- debug_loans_schema.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Schema;
-
-echo "Checking inventory_loans schema:\n";
-$cols = Schema::getColumnListing('inventory_loans');
-foreach ($cols as $c) {
-    echo "$c: " . Schema::getColumnType('inventory_loans', $c) . "\n";
-}
