c86e4a71c74577c1ead7c5a38d6da1495b4a506d
getachewabebe <gechachin@gmail.com>
2026-02-27 20:04:27 +0300

    Fix styling in public/index.php to satisfy Pint

--- public/index.php
+++ public/index.php
@@ -10,10 +10,10 @@ define('LARAVEL_START', microtime(true));
 // 1. Resolve & Check Autoloader
 $autoloader = __DIR__.'/../vendor/autoload.php';
 
-if (! file_exists($autoloader)) {
+if (!file_exists($autoloader)) {
     header('HTTP/1.1 503 Service Unavailable');
     header('Retry-After: 300'); // 5 minutes
-    echo "<h1>System Maintenance</h1><p>The application is currently rebuilding its dependencies. Please refresh in a few moments.</p>";
+    echo '<h1>System Maintenance</h1><p>The application is currently rebuilding its dependencies. Please refresh in a few moments.</p>';
     exit;
 }
 
