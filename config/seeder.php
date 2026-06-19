b8b8b978b19c27109fce235aea5a36e4f52dc0d1
GetachewAbebe <gechachin@gmail.com>
2026-06-19 15:05:00 +0300

    Selam ena Tsetita

--- config/seeder.php
+++ config/seeder.php
@@ -10,18 +10,18 @@
  */
 return [
     // Admin account
-    'admin_email'    => env('ADMIN_DEFAULT_EMAIL', 'administrator@natanemengineering.com'),
+    'admin_email' => env('ADMIN_DEFAULT_EMAIL', 'administrator@natanemengineering.com'),
     'admin_password' => env('ADMIN_DEFAULT_PASSWORD', 'AdminNatanem@123'),
 
     // HR Manager account
-    'hr_email'    => env('HR_DEFAULT_EMAIL', 'humanresource@natanemengineering.com'),
+    'hr_email' => env('HR_DEFAULT_EMAIL', 'humanresource@natanemengineering.com'),
     'hr_password' => env('HR_DEFAULT_PASSWORD', 'HumanResource@123'),
 
     // Inventory Manager account
-    'inventory_email'    => env('INVENTORY_DEFAULT_EMAIL', 'inventorymanager@natanemengineering.com'),
+    'inventory_email' => env('INVENTORY_DEFAULT_EMAIL', 'inventorymanager@natanemengineering.com'),
     'inventory_password' => env('INVENTORY_DEFAULT_PASSWORD', 'InventoryManager@123'),
 
     // Finance Manager account
-    'finance_email'    => env('FINANCE_DEFAULT_EMAIL', 'financialmanager@natanemengineering.com'),
+    'finance_email' => env('FINANCE_DEFAULT_EMAIL', 'financialmanager@natanemengineering.com'),
     'finance_password' => env('FINANCE_DEFAULT_PASSWORD', 'FinancialManager@123'),
 ];
